2de974cdea70f4b0c8a2763724a999fd09bd686e
MATTHEW SIMMS <M.PARKER.SIMMS@GMAIL.COM>
2021-03-23 19:03:40 -0500

    code challenge 2 complete

---
+++ challenges2/lib/src/main/java/challenges2/ArrayShift.java
@@ -0,0 +1,34 @@
+package challenges2;
+
+public class ArrayShift {
+    public static int[] insertShiftArray(int[] intArray, int num){
+        int newArrayLength = (intArray.length + 1);
+        System.out.println(newArrayLength);
+        int index = (newArrayLength / 2);
+        int[] newArray = new int[newArrayLength];
+        System.out.println(index);
+        for( int i =0; i < newArray.length; i++){
+            if(i< index){
+                newArray[i]=intArray[i];
+
+            }else if(i== index){
+                newArray[i] = num;
+            }else{
+                newArray[i] = intArray[i-1];
+            }
+            System.out.println(newArray[i]);
+
+        }
+        System.out.println(newArray);
+
+        System.out.println("this is the middle");
+        System.out.println(newArray[index]);
+
+        return newArray;
+    }
+
+
+
+
+
+}
