57bd726aa5da139fc8c3119085e3a4221b986943
MATTHEW SIMMS <M.PARKER.SIMMS@GMAIL.COM>
2021-04-05 09:53:57 -0500

    code challenge complete

---
+++ stacksandqueues/app/src/main/java/stacksandqueues/Node.java
@@ -0,0 +1,17 @@
+package stacksandqueues;
+
+
+
+
+public class Node {
+
+    Object value;
+    Node next;
+
+    Node(Object data, Node node){
+        value = data;
+        next = node;
+
+    }
+
+}
