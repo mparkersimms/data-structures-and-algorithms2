c76dcca5eec71234b2c32f2b0f0e6299e2296670
MATTHEW SIMMS <M.PARKER.SIMMS@GMAIL.COM>
2021-06-10 18:16:39 -0500

    perfect power

---
+++ utilities/app/src/main/java/utilities/PerfectPower.java
@@ -0,0 +1,38 @@
+package utilities;
+
+//*Description:
+//A perfect power is a classification of positive integers:
+//
+//In mathematics, a perfect power is a positive integer that can be expressed
+// as an integer power of another positive integer. More formally, n is a
+// perfect power if there exist natural numbers m > 1, and k > 1 such that mk = n.
+//
+//Your task is to check wheter a given integer is a perfect power. If it is a
+// perfect power, return a pair m and k with mk = n as a proof. Otherwise return
+// Nothing, Nil, null, NULL, None or your language's equivalent.
+//
+//Note: For a perfect power, there might be several pairs.
+// For example 81 = 3^4 = 9^2, so (3,4) and (9,2) are valid solutions.
+// However, the tests take care of this, so if a number is a perfect power,
+// return any pair that proves it.
+//
+//Examples
+//isPerfectPower(4) => new int[]{2,2}
+//isPerfectPower(5) => null
+//isPerfectPower(8) => new int[]{2,3}
+//isPerfectPower(9) => new int[]{3,2}
+
+public class PerfectPower {
+
+    public static int[] isPerfectPower(int n) {
+        for(int i = 2; i<=400; i++){
+            for(int j = 2; j <=400; j++){
+                double result = Math.pow(i,j);
+                System.out.println("result = " + result);
+                if(result == n) return new int[]{i,j};
+
+            }
+        }
+        return null;
+    }
+}
